3d97b648cadd07822a994a053f8b69241b7e7a04
Jvos <john.voskuyl@gmail.com>
2025-05-16 22:43:35 +0200

    wrote out original pplan for redeploy script

---
+++ redeploy_on_render.sh
@@ -0,0 +1,9 @@
+# get current postgres instance
+# delete current postgres instance
+# create new postgres instance (DB name from env var)
+
+# wait??? or maybe employ a retry loop that try to fetch DB address
+
+# go to each project and replace env var for internal DB address with newly fetched address
+
+# wait???? maybe run a get request for each project and see if we get a 200 back?
