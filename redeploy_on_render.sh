4f18bc933cc232ae743dc2239728e7bf882a602c
Jvos <john.voskuyl@gmail.com>
2025-05-18 14:22:13 +0200

    broke out main delete pg instance step into a function

--- redeploy_on_render.sh
+++ redeploy_on_render.sh
@@ -5,20 +5,29 @@ set -a
 source "$(dirname "$0")/.env"
 set +a
 
-# Get current postgres instance
-pg_instance_response=$(curl --request GET \
-    --url "https://api.render.com/v1/postgres?name=$POSTGRES_INSTANCE_NAME&includeReplicas=true&limit=20" \
-    --header "accept: application/json" \
-    --header "authorization: Bearer $RENDER_REDEPLOY_KEY")
+delete_pg_instance() {
+    # Get current postgres instance
+    echo "Getting current pg instance..."
+    pg_instance_response=$(curl --request GET \
+        --url "https://api.render.com/v1/postgres?name=$POSTGRES_INSTANCE_NAME&includeReplicas=true&limit=20" \
+        --header "accept: application/json" \
+        --header "authorization: Bearer $RENDER_REDEPLOY_KEY")
 
-# need to add error handling here to check if nothing came back and or if the response was unauthorized
+    # need to add error handling here to check if nothing came back and or if the response was unauthorized
 
-pg_instance_id=$(echo "$pg_instance_response" | jq -r '.[].postgres.id')
+    pg_instance_id=$(echo "$pg_instance_response" | jq -r '.[].postgres.id')
 
-echo "Current Postgres instance id: $pg_instance_id"
+    echo "Current Postgres instance id: $pg_instance_id"
 
-# delete current postgres instance
-# create new postgres instance (DB name from env var)
+    # Delete current postgres instance
+}
+
+create_new_pg_instance() {
+    # Create new postgres instance (DB name from env var)
+    echo "Creating new pg instance..."
+}
+
+delete_pg_instance
 
 # wait??? or maybe employ a retry loop that try to fetch DB address
 
