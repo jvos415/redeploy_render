6e69f6eb84780eca0dad804fc6a092545d4447c4
Jvos <john.voskuyl@gmail.com>
2025-05-18 15:26:39 +0200

    completed create new pg instance function and added some more env vars

--- redeploy_on_render.sh
+++ redeploy_on_render.sh
@@ -5,13 +5,13 @@ set -a
 source "$(dirname "$0")/.env"
 set +a
 
-delete_pg_instance() {
+delete_current_pg_instance() {
     # Get current postgres instance
     echo "Getting current pg instance..."
     pg_instance_response=$(curl --request GET \
         --url "https://api.render.com/v1/postgres?name=$POSTGRES_INSTANCE_NAME&includeReplicas=true&limit=20" \
         --header "accept: application/json" \
-        --header "authorization: Bearer $RENDER_REDEPLOY_KEY")
+        --header "authorization: Bearer $RENDER_API_KEY")
 
     # Check if the response is empty
     if [ -z "$pg_instance_response" ]; then
@@ -39,23 +39,53 @@ delete_pg_instance() {
     delete_response=$(curl --request DELETE \
         --url "https://api.render.com/v1/postgres/$pg_instance_id" \
         --header "accept: application/json" \
-        --header "authorization: Bearer $RENDER_REDEPLOY_KEY")
+        --header "authorization: Bearer $RENDER_API_KEY")
 
     if [ -z "$delete_response" ]; then
         echo "Postgres instance deleted successfully."
         exit 0
     fi
 
-    echo "Failed to delete postgres instance."
+    echo "Failed to delete postgres instance: $delete_response"
     exit 1
 }
 
 create_new_pg_instance() {
     # Create new postgres instance (DB name from env var)
-    echo "Creating new pg instance..."
+    echo "Creating new pg instance with the name '$POSTGRES_INSTANCE_NAME'..."
+
+    create_response=$(
+        curl --request POST \
+            --url https://api.render.com/v1/postgres \
+            --header "accept: application/json" \
+            --header "authorization: Bearer $RENDER_API_KEY" \
+            --header "content-type: application/json" \
+            --data "{
+                \"enableHighAvailability\": false,
+                \"plan\": \"free\",
+                \"name\": \"$POSTGRES_INSTANCE_NAME\",
+                \"ownerId\": \"$RENDER_WORKSPACE_ID\",
+                \"version\": \"$POSTGRES_VERSION\"
+                }"
+    )
+
+    # Handle empty response
+    if [ -z "$create_response" ]; then
+        echo "Failed to create new postgres instance."
+        exit 1
+    fi
+
+    # if the response has message then we know there was an error
+    if echo "$create_response" | grep -q "message"; then
+        echo "Error creating new postgres instance: $create_response"
+        exit 1
+    fi
+
+    echo "New pg instance created successfully."
+    exit 0
 }
 
-delete_pg_instance
+create_new_pg_instance
 
 # wait??? or maybe employ a retry loop that try to fetch DB address
 
